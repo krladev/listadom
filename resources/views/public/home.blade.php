756438b227cb674a82e8bc172b65ce92af97c507
Karla Navas <dev@karlanavas.com>
2021-08-11 00:38:19 +1000

    adding the 4 steps to home page

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -1,14 +1,41 @@
 @extends('layouts.app')
 @section('title', 'Home')
 
-@section('header')
-    @parent
-@endsection
-
 @section('content')
     <div class="md:container md:mx-auto">
-        <h1>Welcome to List-A-Dom</h1>
+        <h1 class="text-center mt-4">List-A-Dom</h1>
 
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia aut veritatis optio. Voluptatibus sit nisi nulla modi est corrupti animi quam maiores explicabo possimus, vero fugiat sed voluptatem facere. Impedit!</p>
-    </div>  
+        <p class="quote text-center mt-4 mb-4"">Easily sell your domain in 4 steps</p>
+
+        <div class="flex flex-wrap overflow-hidden">
+            <div class="w-full overflow-hidden sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="card-buy-step">
+                    <div><i class="fas fa-user-plus"></i></div>
+                    <div>Create an account</div>
+                </div>
+            </div>
+
+            <div class="w-full overflow-hidden sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="card-buy-step">
+                    <div><i class="fas fa-cog"></i></div>
+                    <div>Setup your domain</div>
+                </div>
+            </div>
+
+            <div class="w-full overflow-hidden sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="card-buy-step">
+                    <div><i class="fas fa-palette"></i></div>
+                    <div>Look and feel</div>
+                </div>
+            </div>
+
+            <div class="w-full overflow-hidden sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="card-buy-step">
+                    <div><i class="fas fa-comment-dollar"></i></div>
+                    <div>Get bids</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection
