613d5d60902a8d9cf3bccdaaff6219d4ea24e69b
Karla Navas <dev@karlanavas.com>
2021-08-17 06:19:17 +1000

    login page

---
+++ resources/views/public/login.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('title', 'Login')
+
+@section('content')
+    <div class="md:container md:mx-auto page-login">
+        <form id="form-login" class="bg-white shadow-md rounded px-8 pt-3 pb-8 mb-4">
+            <h1 class="text-center mt-4">List-A-Dom</h1>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                Email
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email">
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                Password
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                {{-- <p class="text-red-500 text-xs italic">Please choose a password.</p> --}}
+            </div>
+            <div class="flex items-center justify-between">
+                <button class="btn-cta btn-primary" type="button">
+                Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/forgot">
+                    Forgot Password?
+                </a>
+            </div>
+        </form>
+
+    </div>
+@endsection
