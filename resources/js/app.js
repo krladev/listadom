c361c09a606e0ff9e44220a72050fb0b256bd0f5
Karla Navas <dev@karlanavas.com>
2021-08-17 06:19:34 +1000

    js setup for jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+$(function(){
+    // $('body').on('click', '.page-home .btn-primary', function() {
+
+    // });
+});
