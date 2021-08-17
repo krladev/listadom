860b1f44d212ea1b156a031bede80626551d1493
Karla Navas <dev@karlanavas.com>
2021-08-18 06:33:24 +1000

    minor UI update to include error message

--- resources/views/public/auth/login.blade.php
+++ resources/views/public/auth/login.blade.php
@@ -10,13 +10,13 @@
                 Email
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email">
+                <p id="error-message" class="text-red-500 text-xs italic mt-1"></p>
             </div>
             <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                 Password
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-                {{-- <p class="text-red-500 text-xs italic">Please choose a password.</p> --}}
             </div>
             <div class="flex items-center justify-between">
                 <button class="btn-cta btn-primary" type="submit">
